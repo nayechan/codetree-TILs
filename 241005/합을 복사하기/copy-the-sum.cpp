70afe63485128d9d514273140a7094cbf85879a4
nayechan <66f@naver.com>
2024-10-05 14:58:42 +0900

    Title: 합을 복사하기, Time: 6ms, Memory: 0MB, Status: Passed - CodeTree

---
+++ 241005/합을 복사하기/copy-the-sum.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 1, b = 2, c = 3;
+    int sum = a+b+c;
+    a = sum;
+    b = sum;
+    c = sum;
+    cout << a << " " << b << " " << c << endl;
+    return 0;
+}
